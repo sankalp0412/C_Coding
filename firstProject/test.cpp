7b58fc892e110d4c502eb3b35245f837cdf571c2
sankalp0412 <dhuparsankalp@gmail.com>
2023-06-19 08:09:32 +0530

    First Commit

---
+++ firstProject/test.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void init_code() {
+	#ifndef ONLINE_JUDGE
+	
+freopen("input.txt", "r", stdin);
+
+freopen("output.txt", "w", stdout);
+#endif
+}
+
+int main() {
+	init_code();
+	int x;
+	cin >> x;
+	cout << "you numbessr" << " " << x;
+	return 0;
+}
