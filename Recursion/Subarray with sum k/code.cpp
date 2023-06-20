0a6ef4e222d015a6b401b1abdc22570074e986f1
sankalp0412 <dhuparsankalp@gmail.com>
2023-06-20 16:15:08 +0530

    Subarry with sum k - Technique to print only one answer using bool functions

--- Recursion/Subarray with sum k/code.cpp
+++ Recursion/Subarray with sum k/code.cpp
@@ -9,24 +9,29 @@ freopen("output.txt", "w", stderr);
 #endif
 }
 
-void fn(vector<int> &vec, int n,int curr, int sum, int index,vector<int> &temp) {
+bool fn(vector<int> &vec, int n,int curr, int sum, int index,vector<int> &temp) {
 	if(index == n) {
+		//condition sattisfied
 		if(curr == sum) {
-			for(auto it: temp){
-				cout << it << " ";
-			}
+			for(auto it: temp) cout << it << " ";
 				cout << endl;
+				return true;
 		}
-		return;
+		//not satisfied
+		else return false;
 	}
 	temp.push_back(vec[index]);
 	curr += vec[index];
 	//take
-	fn(vec,n,curr,sum,index+1,temp);
+	if (fn(vec,n,curr,sum,index+1,temp))	
+		return true;
 	curr -= vec[index];
 	temp.pop_back();
 	//not take
-	fn(vec,n,curr,sum,index+1,temp);
+	if(fn(vec,n,curr,sum,index+1,temp))
+		return true;
+
+	return false;
 }
 int main (){
 	init_code();
@@ -37,3 +42,23 @@ int main (){
 	return 0;
 
 }
+
+
+//NOW LETS SAY IF WE MODIFY THE QUESTION  AND SAY PRINT ONLY ONE SEQUECEN
+// WE NEED TO END THE RECURSION IMMEDIATELY IF WE GET AN ANSWER SUCH A WAY THAT 
+
+//TRICK TO PRINT ONE ANSWER:
+
+//bool F(){
+// BASE CASE:
+// 	IF CONDITION SATISFIED
+// 		RETURN TRUE
+// 	ELSE 
+// 		RETURN FALSE
+
+// IF(F() == TRUE)
+// 	RETURN true;
+
+// F();
+//return false
+// }
