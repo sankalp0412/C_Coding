cefc52af9be1354d176c7024221e380a93e9d38a
sankalp0412 <dhuparsankalp@gmail.com>
2023-07-22 15:39:52 +0530

    Rearrange element by size

--- Array Playlist/Rearrange array elements by size/code.cpp
+++ Array Playlist/Rearrange array elements by size/code.cpp
@@ -8,8 +8,31 @@ freopen("output.txt", "w", stdout);
 freopen("output.txt", "w", stderr);
 #endif
 }
-
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n,0);
+        int pos = 0, neg = 1;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] < 0){
+                ans[neg] = nums[i];
+                neg += 2;
+            }
+            else {
+                ans[pos] = nums[i];
+                pos += 2;
+            }
+        }
+        return ans;
+    }
+};
 int main() {
 	init_code();
+	Solution solution;
+	vector<int> nums = {3,1,-2,-5,2,-4};
+	nums = solution.rearrangeArray(nums);
+	for(auto x: nums)
+		cout << x << endl;
 	return 0;
 }
