3e54ddea6d41cad4656229c36205fb561dad8d93
sankalp0412 <dhuparsankalp@gmail.com>
2023-07-29 13:54:25 +0530

    Majority element 2

---
+++ Array Playlist/majority element 2/code.cpp
@@ -0,0 +1,60 @@
+//question: return all elements that appear more than n/3 time
+
+//Intution: 
+//at max only 2 integers can be part of the answer
+
+
+//modifying moores voting algo slightl for two element
+#include "bits/stdc++.h"
+using namespace std;
+
+void init_code() {
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+freopen("output.txt", "w", stderr);
+#endif
+}
+vector<int> majorityelement2(vector<int> nums) {
+	int cnt1 = 0, cnt2 = 0;
+	int n = nums.size();
+	int el1 = INT_MIN, el2 = INT_MIN;
+	for(int i = 0; i < n; i ++) {
+		if(cnt1 == 0 && el2 != nums[i]){
+			cnt1 = 1;
+			el1 = nums[i];
+		}
+		else if(cnt2 == 0 && el1 != nums[i]){
+			cnt2 = 1;
+			el2 = nums[i];
+		}
+		else if(nums[i] == el1) cnt1++;
+		else if(nums[i] == el2) cnt2++;
+		else{
+			cnt1--;cnt2--;
+		}
+	}
+	vector<int> ans;
+	cnt1 = 0, cnt2 = 0;
+	for(auto x: nums) {
+		if(x == el1) cnt1++;
+		if(x == el2) cnt2++;
+	}
+	int mini = (int)n/3 + 1;
+	if(cnt1 >= mini) ans.push_back(el1);
+	if(cnt2 >= mini) ans.push_back(el2);
+	sort(ans.begin(),ans.end());
+	return ans;
+}
+
+int main() {
+	init_code();
+	vector<int> nums = {3,2,3};
+	vector<int> ans = majorityelement2(nums);
+	for(auto x: ans) cout<<x << " ";
+	return 0;
+}
+
+
+//tc: o(2n);
+//sc: O(1); 
