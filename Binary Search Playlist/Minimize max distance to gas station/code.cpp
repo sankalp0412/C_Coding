fe28d70ee7a3670e70d2b33b7a512594238c7e7a
sankalp0412 <dhuparsankalp@gmail.com>
2023-09-16 14:32:37 +0530

    Gas Station optimal

--- Binary Search Playlist/Minimize max distance to gas station/code.cpp
+++ Binary Search Playlist/Minimize max distance to gas station/code.cpp
@@ -55,13 +55,41 @@ long double minimiseMaxDistanceBetter(vector<int> &arr, int &k) {
 	return pq.top().first;
 }
 
+int numberOfGasStationRequired(long double dist, vector<int> &arr) {
+	int cnt = 0;
+	for(int i = 1; i < arr.size(); i++) {
+		int numberInBetween = ((arr[i] - arr[i-1])/dist);
+		if((arr[i] - arr[i-1]) / dist == numberInBetween * dist){
+			numberInBetween--;
+		}
+		cnt += numberInBetween;
+	}
+	return cnt;
+}
 long double minimiseMaxDistanceOptimal(vector<int> &arr, int &k) {
-	
+	int n = arr.size();
+	long double low = 0;
+	long double high = 0;
+	for(int i = 0; i < n-1; i++) {
+		high = max(high,(long double)arr[i+1] - arr[i]);
+	}
+	long double diff = INT_MIN;
+	while(high - low > diff) {
+		long double mid = (low + high)/(2.0);
+		int cnt = numberOfGasStationRequired(mid,arr);
+		if(cnt > k) {
+			low = mid;
+		}
+		else {
+			high = mid;
+		}
+	}
+	return high;
 }
 int main() {
 	init_code();
 	vector<int> arr = {1,2,3,4,5};
 	int k =	4;
-	cout << minimiseMaxDistanceBetter(arr,k);
+	cout << minimiseMaxDistanceOptimal(arr,k);
 	return 0;
 }
